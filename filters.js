180d7d99f084ae430b5e8b7b13bba4aa55cbb381
jatin sharma <jatinsharma9871@gmail.com>
2026-01-28 11:27:01 +0530

    comit

--- filters.js
+++ filters.js
@@ -1,9 +1,27 @@
-export default async function handler(req, res) {
-  const { shop } = req.query;
+import products from "./products.json";
 
-  // Example response (replace with real logic)
-  res.status(200).json({
-    success: true,
-    products: []
+export default function handler(req, res) {
+  let result = products;
+
+  const { price_min, price_max, vendor, tag } = req.query;
+
+  if (price_min)
+    result = result.filter(p => p.price >= +price_min);
+
+  if (price_max)
+    result = result.filter(p => p.price <= +price_max);
+
+  if (vendor)
+    result = result.filter(p => vendor.includes(p.vendor));
+
+  if (tag)
+    result = result.filter(p => p.tags.includes(tag));
+
+  res.json({
+    products: result.slice(0, 24),
+    counts: {
+      "vendor:Nike": 120,
+      "vendor:Adidas": 90
+    }
   });
 }
